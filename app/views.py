2651152d195bd22d863831a0babb6c9ae85e8d44
Meron <merronmuche@gmail.com>
2024-01-12 00:39:56 +0300

    better visualization

--- app/views.py
+++ app/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
+from app.models import Trainee
 
 
 from django.shortcuts import render
 
 def view_certificate(request, trainee_id):
-    # Example data - in a real application, you would retrieve this from your database
-    trainee_name = "John Doe"  # You would fetch this using trainee_id
-    challenge_name = "Web3 Development Challenge"
-    nft_id = "NFT123456789"
-    blockchain_url = "https://explorer.algorand.org/transaction/" + nft_id
 
-    # Context to be passed to the template
+    trainee = Trainee.objects.get(pk = trainee_id)
     context = {
-        'trainee_name': trainee_name,
-        'challenge_name': challenge_name,
-        'nft_id': nft_id,
-        'blockchain_url': blockchain_url,
+        'trainee_name': trainee.name,
+        'course_name': trainee.course,
+        'certified_date': trainee.date_certified,
+        'certificate': trainee.certificate
     }
 
     # Render and return the template
