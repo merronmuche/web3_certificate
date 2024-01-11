0c04ab67630e71a534cbd066c0f88521e9a1c462
Meron <merronmuche@gmail.com>
2024-01-11 23:59:05 +0300

    Add a trainee model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Trainee
+
+class TraineeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')
+    search_fields = ('name', 'email')
+    
+
+admin.site.register(Trainee, TraineeAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+from collections.abc import Iterable
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Trainee(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(unique=True)
+    date_certified = models.DateField(auto_now_add=True)
+    certificate = models.ImageField(upload_to='media')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        # open the certificate image using opencv
+        # add the name, course and date to the certificate
+        return super().save(*args, **kwargs)
+
